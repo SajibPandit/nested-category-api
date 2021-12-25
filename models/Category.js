c5b1f05ee7e0c7c9af62cd1356e1e1be4959729f
Sajib Pandit <sajibpandit1999@gmail.com>
2021-12-25 21:08:40 +0600

    Schema modified

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
-const categorySchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    slug: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    parentId: String,
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
   },
-  { timestamps: true }
-);
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  parentId: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("Category", categorySchema);
